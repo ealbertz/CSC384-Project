8f86f97cc217b07127876e0295774c40cffc8790
Emma Albertz <emma.albertz@gmail.com>
2016-12-05 17:38:43 -0500

    Dummy test for fifteenPuzzle

--- eightPuzzle_test.py
+++ eightPuzzle_test.py
@@ -1,6 +1,6 @@
 from puzzle_heuristics import *
 
-problem = EightPuzzleState("START",0,[1,3,6,5,0,2,4,7,8])
+problem = FifteenPuzzleState("START",0,[1,3,6,5,0,2,4,7,8,9,10,11,12,13,14,15])
 
 print("Misplaced tiles: {}".format(heur_displaced(problem)))
 print("Manhattan distance: {}".format(heur_manhattan_distance(problem)))
