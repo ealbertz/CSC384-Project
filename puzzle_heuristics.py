381f65a10d2d7b3c43f315ed3557ab00236d6c6b
Fredrik Wegelid <wegelid@gmail.com>
2016-12-05 14:34:05 -0500

    Fixed Manhattan heuristic and added Out of row/column heuristic

--- puzzle_heuristics.py
+++ puzzle_heuristics.py
@@ -13,15 +13,27 @@ def heur_displaced(state):
 def heur_manhattan_distance(state):
 '''A heuristic function which returns the sum of all Manhattan distances for each tile to its goal position'''
 
-  n = state.state.length - 1 #The width/height of the puzzle
+  n = state.state.length - 1 #The number of tiles in the puzzle
   total_dist = 0
   for i in range (1,n):
-   total_dist += manhattan_dist(state.state.index(i),state.goal_state.index(i),n)
+   total_dist += manhattan_dist(state.state.index(i),state.goal_state.index(i),sqrt(n + 1))
   return total_dist
 
 def manhattan_dist(i,j,n)
 '''Manhattan distance of two indexes i and j in a puzzle with the width/height n'''
   return abs(i % n - j % n) + abs(i // n - j // n)
+
+def heur_out_of_row_and_column(state):
+'''A heuristic function which returns the sum of number of tiles in the wrong row and the number of tiles in the wrong column'''
+  
+  n = state.state.length - 1 #The number of tiles in the puzzle
+  count = 0
+  for i in range(1,n):
+    if state.state.index(i) % sqrt(n) != state.goal_state.index(i) % sqrt(n):
+      count += 1
+    if state.state.index(i) // sqrt(n) != state.goal_state.index(i) // sqrt(n):
+      count += 1
+  return count
 
 def heur_alternate(state):
 #IMPLEMENT
