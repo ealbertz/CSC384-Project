ed1d7660749bd12f9868a723a08188862df174ed
Fredrik Wegelid <wegelid@gmail.com>
2016-12-05 17:16:37 -0500

    Added 18 more problems to the problem set

--- eightPuzzle.py
+++ eightPuzzle.py
@@ -80,11 +80,27 @@ def eightPuzzle_goal_state(state):
     '''Output: True (if goal) or False (if not)'''
     return(state.goal_state == state.state)
 
-    
-
 '''A set of problems for the 8-puzzle'''
 EIGHT_PROBLEMS = (
     EightPuzzleState("START",0,[2,0,4,5,8,3,6,7,1]),
-    EightPuzzleState("START",0,[8,1,2,3,4,0,6,5,7])
+    EightPuzzleState("START",0,[8,1,2,3,4,0,6,5,7]),
+    EightPuzzleState("START",0,[1,0,6,5,8,2,4,3,7]),
+    EightPuzzleState("START",0,[4,6,5,2,1,0,7,3,8]),
+    EightPuzzleState("START",0,[2,0,6,3,4,5,7,8,1]),
+    EightPuzzleState("START",0,[5,8,7,3,4,0,6,2,1]),
+    EightPuzzleState("START",0,[3,7,6,5,4,1,2,0,8]),
+    EightPuzzleState("START",0,[2,3,5,8,7,0,1,6,5]),
+    EightPuzzleState("START",0,[3,2,1,5,7,8,4,0,6]),
+    EightPuzzleState("START",0,[7,8,1,0,6,4,5,2,3]),
+    EightPuzzleState("START",0,[3,0,5,4,8,1,2,7,6]),
+    EightPuzzleState("START",0,[5,1,7,6,2,0,3,8,4]),
+    EightPuzzleState("START",0,[5,1,3,0,2,4,7,6,8]),
+    EightPuzzleState("START",0,[3,6,4,1,7,5,8,0,2]),
+    EightPuzzleState("START",0,[1,5,8,0,2,6,4,7,3]),
+    EightPuzzleState("START",0,[6,0,2,1,5,8,4,3,7]),
+    EightPuzzleState("START",0,[1,2,4,0,8,3,7,6,5]),
+    EightPuzzleState("START",0,[5,2,6,3,8,0,7,1,4]),
+    EightPuzzleState("START",0,[1,0,6,7,3,5,2,8,4]),
+    EightPuzzleState("START",0,[7,3,4,0,6,2,1,5,8])
     )
 
