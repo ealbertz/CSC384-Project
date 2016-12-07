5a1c07e7ba34451b9f44fbc52f45c8d9009a5a82
Fredrik Wegelid <wegelid@gmail.com>
2016-12-07 13:54:35 -0500

    Updated test case to include new heuristics

--- test_script.py
+++ test_script.py
@@ -6,6 +6,6 @@ for i in range(0,20):
 	print("******************************")
 	print("PROBLEM {}".format(i+1))
 	s0 = EIGHT_PROBLEMS[i]
-	se = SearchEngine('best_first', 'full')
-	for h in [heur_displaced, heur_manhattan_distance,heur_out_of_row_and_column]:
-		final = se.search(initState = s0, goal_fn = eightPuzzle_goal_state, heur_fn = h, timebound = 1)
+	se = SearchEngine('custom', 'full')
+	for h in [heur_displaced, heur_manhattan_distance,heur_out_of_row_and_column, heur_manhattan_linear]:
+		final = se.search(initState = s0, goal_fn = eightPuzzle_goal_state, heur_fn = h, timebound = 1, fval_function = fval_function, weight = 0.5)
