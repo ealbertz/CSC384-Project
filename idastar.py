275240f60aec44ac58b69736c10f483dcd8203e9
Fredrik Wegelid <wegelid@gmail.com>
2016-12-07 17:17:26 -0500

    The IDA function now returns the total time taken to find the solution

--- idastar.py
+++ idastar.py
@@ -12,16 +12,21 @@ def fval_function(sN, weight):
 
 
 
-def iterative_deepening_astar(initial_state, goal_fn, heur_fn):
+def iterative_deepening_astar(initial_state, goal_fn, heur_fn, timebound):
 
 	se=SearchEngine('depth_first', 'full')
 	curBound=1
-	solution=False;
+	solution=False
+	total_time = 0
 	while(solution == False): #while we are not in a goal node
-		solution = se.search(initState=initial_state, goal_fn = goal_fn, heur_fn = heur_fn, fval_function=fval_function, bound=curBound)
+		solution = se.search(initState=initial_state, goal_fn = goal_fn, heur_fn = heur_fn, fval_function=fval_function, bound=curBound, timebound=timebound)
+		total_time += se.total_search_time
 		if(solution == False):
+			if (total_time >= timebound):
+				return False
 			smallestNotExplored=se.smallestNotExplored
 			print(se.smallestNotExplored)
 			curBound=smallestNotExplored
-	return solution
+	print("Total time: {}".format(total_time))
+	return solution, total_time
 
