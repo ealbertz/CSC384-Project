0f931da9cfcbd88864e175a28e511fb716b5d2a1
Fredrik Wegelid <wegelid@gmail.com>
2016-12-07 14:44:32 -0500

    Added test cases for 15-puzzles

--- test_script.py
+++ test_script.py
@@ -1,11 +1,20 @@
 from puzzle_heuristics import *
 from eightPuzzle import *
+from fifteenPuzzle import *
 
 
 for i in range(0,20):
 	print("******************************")
 	print("PROBLEM {}".format(i+1))
 	s0 = EIGHT_PROBLEMS[i]
-	se = SearchEngine('custom', 'full')
+	se = SearchEngine('astar', 'full')
 	for h in [heur_displaced, heur_manhattan_distance,heur_out_of_row_and_column, heur_manhattan_linear]:
-		final = se.search(initState = s0, goal_fn = eightPuzzle_goal_state, heur_fn = h, timebound = 1, fval_function = fval_function, weight = 0.5)
+		final = se.search(initState = s0, goal_fn = eightPuzzle_goal_state, heur_fn = h, timebound = 1)
+
+for i in range(0,5):
+	print("******************************")
+	print("PROBLEM {}".format(i+1))
+	s0 = FIFTEEN_PROBLEMS[i]
+	se = SearchEngine('astar', 'full')
+	for h in [heur_displaced, heur_manhattan_distance,heur_out_of_row_and_column, heur_manhattan_linear]:
+		final = se.search(initState = s0, goal_fn = eightPuzzle_goal_state, heur_fn = h, timebound = 5)
