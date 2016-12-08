9c85cacf657ddff070e5f14d074126503919e7e1
Fredrik Wegelid <wegelid@gmail.com>
2016-12-08 12:11:28 -0500

    IDA* no longer exceeds the maximum timebound

--- idastar.py
+++ idastar.py
@@ -2,31 +2,24 @@ from search import * #for search engines
 from eightPuzzle import *
 from fifteenPuzzle import *
 
-def fval_function(sN, weight): 
-
-  gval=sN.gval
-  hval=sN.hval
-  fval= gval+hval
-  
-  return fval
-
-
-
 def iterative_deepening_astar(initial_state, goal_fn, heur_fn, timebound):
 
-	se=SearchEngine('depth_first', 'full')
-	curBound=1
-	solution=False
+	se = SearchEngine('depth_first', 'full')
+
+	current_bound = 1
+	current_timebound = timebound
+	solution = False
 	total_time = 0
-	while(solution == False): #while we are not in a goal node
-		solution = se.search(initState=initial_state, goal_fn = goal_fn, heur_fn = heur_fn, fval_function=fval_function, bound=curBound, timebound=timebound)
+
+	while (current_timebound): #while we are not in a goal node
+		solution = se.search(initState = initial_state, goal_fn = goal_fn, heur_fn = heur_fn, bound = current_bound, timebound = current_timebound)
+
 		total_time += se.total_search_time
-		if(solution == False):
-			if (total_time >= timebound):
-				return False, total_time
-			smallestNotExplored=se.smallestNotExplored
-			print(se.smallestNotExplored)
-			curBound=smallestNotExplored
-	print("Total time: {}".format(total_time))
-	return solution, total_time
+		current_timebound = max(0, current_timebound - se.total_search_time)
+		current_bound = se.smallestNotExplored
+
+		if (solution):
+			return solution, total_time
+		
+	return False, total_time
 
